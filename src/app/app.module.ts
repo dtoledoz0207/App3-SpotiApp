79bc187305d84e3638f492f4c95989546a0eaaf3
David Toledo <dtoledoz0207@gmail.com>
2019-01-15 11:54:52 -0600

    componentsHomSeaArtNav-Generated-routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Esta linea es para las rutas como se crearon en el curso
+import {RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// IMPORTAR RUTAS
+// Esta linea es para las rutas como se crearon en el curso
+import {ROUTES} from './app.routes';
+
+/* Importacion de rutas creadas con la ayuda de Angular Snipets 
+    de esta forma en el imports solo se coloca AppRoutingModule
+*/
+//import {AppRoutingModule} from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,35 @@
+import {Routes} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {SearchComponent} from './components/search/search.component';
+
+
+export const ROUTES: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'search', component: SearchComponent},
+    {path: '', pathMatch: 'full', redirectTo: 'home'},
+    {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+
+
+
+// ***** Esta creacion de rutas se hace con la ayuda de Angular Snipets ****
+/*import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import {HomeComponent} from './components/home/home.component';
+import {SearchComponent} from './components/search/search.component';
+
+const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'search', component: SearchComponent },
+    { path: '**', component: HomeComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(ROUTES, {useHash: true})],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}*/
+// **************************************************************************
